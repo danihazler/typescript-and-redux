981ab9aa2e3d024805f3aaa9e124d7bea16b1f7c
Dani <danihazler@gmail.com>
2021-07-21 14:26:01 +0100

    Add the design

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Value here</h1>
+      <div className="buttons">
+        <button onClick={() => console.log('deposit')}>Deposit</button>
+        <button onClick={() => console.log('withdraw')}>Withdraw</button>
+        <button onClick={() => console.log('banckrupt')}>Bankrupt</button>
+      </div>
     </div>
   );
 }
